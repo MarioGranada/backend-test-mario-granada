ff71e7a48923d6fe8ef0e2715dde2b5669bc6d37
MarioGranada <mario.granada190@gmail.com>
2016-03-22 17:43:37 -0500

    up

--- scripts/services/services.js
+++ scripts/services/services.js
@@ -7,8 +7,24 @@ services.factory("RappiMatrix",function(){
 
     var RappiMatrix=function(){
         this.matrix=null;
+        this.size=0;
     }
 
+    RappiMatrix.prototype.reconstructMatrix=function(n){
+        this.size=n;
+    }
+    RappiMatrix.prototype.getValue=function(x,y,z){
+
+    }
+    RappiMatrix.prototype.setValue=function(x,y,z,w){
+
+    }
+    RappiMatrix.prototype.sumPoints=function(x1,y1,z1,x2,y2,z2){
+
+    }
+    RappiMatrix.prototype.size=function(){
+        return this.size;
+    }
     return RappiMatrix;
 
 });
