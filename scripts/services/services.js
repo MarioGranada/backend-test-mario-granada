f218215ba45b68d58e6f015a561639337a6a2aa3
MarioGranada <mario.granada190@gmail.com>
2016-03-22 19:06:16 -0500

    gui

--- scripts/services/services.js
+++ scripts/services/services.js
@@ -12,15 +12,32 @@ services.factory("RappiMatrix",function(){
 
     RappiMatrix.prototype.reconstructMatrix=function(n){
         this.size=n;
+        this.matrix= new Array(n);
+        for (var i = 0; i < n; i++) {
+            this.matrix[i] = new Array(n);
+            for (var j = 0; j < n; j++) {
+                this.matrix[i][j] = new Array(n);
+                for (var z = 0; z < n; z++) {
+                    this.setValue(i,j,z,0);
+                }
+            }
+        }
     }
     RappiMatrix.prototype.getValue=function(x,y,z){
-
+        return this.matrix[x][y][z];
     }
     RappiMatrix.prototype.setValue=function(x,y,z,w){
-
+        this.matrix[x][y][z]=w;
     }
     RappiMatrix.prototype.sumPoints=function(x1,y1,z1,x2,y2,z2){
-
+        var sum=0;
+        for ( i=0; x1 <= x2; i++) {
+            sum+=this.matrix[x1][y1][z1];
+            x1++;
+            y1++;
+            z1++;
+        };
+        return sum;
     }
     RappiMatrix.prototype.size=function(){
         return this.size;
