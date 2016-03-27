19e650609d39a0672f68fd29de26205f074741c3
MarioGranada <mario.granada190@gmail.com>
2016-03-26 20:14:18 -0500

    code_refactoring

---
+++ part-two-backend-test.php
@@ -0,0 +1,34 @@
+public function post_confirm(){
+	$service_id = Input::get('service_id');
+	$driver_id=Input::get('driver_id');
+	$service= Service::find($id);
+	
+	if ($service->status_id == '6'){
+		return Response::json(array('error' => '2'));
+	}
+
+	if ($service->driver_id == NULL && $service->statud_id == '1'){
+		$service = Service::update($service_id, array(
+					'driver_id' => $driver_id,
+					'status_id' => '2'
+		));
+		Driver::update($driver_id,array(
+			"available" => '0'
+		));
+		$pushMessage = 'Tu servicio ha sido confirmado!'; //Notificar a usuario!!
+
+		$push = Push::make();
+		if ($service->user->uuid == ''){
+			return Response::json(array('error' => '0'));
+		}
+		if ($service->user->type == '1'){//iPhone
+			$result= $push->ios($service->user->uuid,$pushMessage,1, 'honk.wav', 'Open', array('serviceId' => $service_id));
+		} else {
+			$result = $push-> android2($service->user->uuid,$pushMessage,1,'default','Open',array('serviceId' => $service_id));
+		}
+		return Response::json(array('error' => '0'));
+	}else{
+		return Response::json(array('error' => '1'));
+	}
+	
+}
